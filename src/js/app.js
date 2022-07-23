238e87d2ebf840049e8d549edea4d9b33534ca8a
Ece <ecemuharremoglu1998@gmail.com>
2022-07-23 14:02:08 +0300

    finish html

--- src/js/app.js
+++ src/js/app.js
@@ -11,10 +11,12 @@ App = {
       for (i = 0; i < data.length; i ++) {
         petTemplate.find('.panel-title').text(data[i].name);
         petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+        petTemplate.find('.kw').text(data[i].kw);
+        petTemplate.find('.type').text(data[i].type);
+        petTemplate.find('.time').text(data[i].time);
+        petTemplate.find('.total').text(data[i].total);
+        petTemplate.find('.location').text(data[i].location);
+        petTemplate.find('.btn-getoffers').attr('data-id', data[i].id);
 
         petsRow.append(petTemplate.html());
       }
